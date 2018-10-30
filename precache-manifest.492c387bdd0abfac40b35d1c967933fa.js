eac64af8b568b6c1ea26489689ca3e76102581b1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-30 01:24:59 +0000

    Deploy triglian/onedot-demo to github.com/triglian/onedot-demo.git:gh-pages

---
+++ precache-manifest.492c387bdd0abfac40b35d1c967933fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "44f7c6aa114eb45b936b",
+    "url": "/onedot-demo/static/css/main.4b399fa7.chunk.css"
+  },
+  {
+    "revision": "44f7c6aa114eb45b936b",
+    "url": "/onedot-demo/static/js/main.44f7c6aa.chunk.js"
+  },
+  {
+    "revision": "9e376c2ecc6054083d81",
+    "url": "/onedot-demo/static/js/1.9e376c2e.chunk.js"
+  },
+  {
+    "revision": "a142fcf366a95d4367f4",
+    "url": "/onedot-demo/static/js/runtime~main.a142fcf3.js"
+  },
+  {
+    "revision": "b6e2640d08c6c18c2ba917ea563bcc3c",
+    "url": "/onedot-demo/index.html"
+  }
+];
